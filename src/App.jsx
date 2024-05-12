d963f3bd963957f685ec923032eb7a1325ea5e14
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-05-12 13:02:17 +0600

    Create Blog Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Blogs from "./pages/Blogs";
+import CreateBlog from "./pages/CreateBlog";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
@@ -12,6 +13,8 @@ const App = () => {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs" element={<CreateBlog />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -26,10 +26,7 @@ const Blogs = () => {
             <img src={blog.image} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{blog.title}</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up the bulk of the cards
-                content.
-              </p>
+              <p className="card-text">{blog.description}</p>
               <a href="#" className="btn btn-primary">
                 Details
               </a>
